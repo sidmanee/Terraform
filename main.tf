5308b9b242463022ec8e7d0d146714267a6d65d2
sidmanee <siddharthmane23@gmail.com>
2025-12-12 10:41:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "local" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-cicd-demo-rg"
+  location = "eastus"
+}
+
+# Storage Account
+resource "azurerm_storage_account" "storage" {
+  name                     = "tfcicddemost${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+}
 
